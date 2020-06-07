c35b7c13201965340fe8cccd4d861dc1e0f8fd67
Robin Lunde <robin@wub.no>
2020-06-07 05:51:05 +0200

    Change getJS to limit output

--- support/getJS.sh
+++ support/getJS.sh
@@ -8,7 +8,7 @@ NAME=$(echo "$1" | awk -F/ '{print $3}')
 NAMEJSFOLDER="$4/$NAME"
 mkdir -p "$NAMEJSFOLDER"
 
-python3 "$2" -u "$1" -n "$3" | tee -a "$NAMEJSFOLDER/$NAME.txt"
+python3 "$2" -u "$1" -n "$3" >> "$NAMEJSFOLDER/$NAME.txt" 2>&1
 
 if [ -z "$(ls -A "$NAMEJSFOLDER/")" ]; then
   rmdir "$NAMEJSFOLDER"
